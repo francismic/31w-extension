3cc9e876fb06110e1b01f86522dbd7ebcac9958e
Francis Michaud <89165460+francismic@users.noreply.github.com>
2022-10-28 09:56:46 -0400

    c9 Ajout du carrousel

--- fm_carrousel.php
+++ fm_carrousel.php
@@ -13,17 +13,48 @@
  * 
  */
 
+   ## filemtime() // retourne en milliseconde le temps de la dernière sauvegarde
+## plugin_dir_path() // retourne le chemin du répertoire du plugin
+## __FILE__ // le fichier en train de s'exécuter
+## wp_enqueue_style() // Intègre le link:css dans la page
+## wp_enqueue_script() // intègre le script dans la page
+## wp_enqueue_scripts // le hook
+
+function fm_enqueue(){
+
+    $version_css = filemtime(plugin_dir_path(__file__). "style.css");
+    $version_js = filemtime(plugin_dir_path(__file__). "js/carrousel.js");
+    /* var_dump(__FILE__); die(); */
+    wp_enqueue_style("fm_carrousel",plugin_dir_url( __FILE__)  . "style.css",
+    array(),
+    $version_css,
+    false);
+
+wp_enqueue_script("fm_carrousel", plugin_dir_url( __FILE__) . "js/carrousel.js",
+                array(),
+                $version_js,
+                true);
+    
+};
+
+add_action("wp_enqueue_scripts","fm_enqueue");
+
+
 function boite_carrousel(){
     /////////////////////////////////////// HTML
     // Le conteneur d'une boîte
     $contenu = 
-    "<section class='carrousel'>"
-    .'<button class = "carrousel--fermer">X<button>'
-    .'<figure class="carrousel__figure"></figure>'
-    .'<form class="carrousel__form"></form>'
-    . '</section> <!-- fin class="carrousel" -->';
-    
-    return $contenu;
+        "
+        <button class='bouton'>Ouvrir le carrousel</button>
+        <div class='carrousel'>"
+
+            // --fermer parce que ACTION __figure parce que élément se trouve dans carrousel
+            .'<button class="carrousel__x">X</button>'
+            .'<figure class="carrousel__figure"></figure>'
+            .'<form class="carrousel__form"></form>'
+        .'</div> <!-- fin class="carrousel" -->';
+        
+        return $contenu;
    }
    add_shortcode('fm_carrousel', 'boite_carrousel');
 
--- js/carrousel.js
+++ js/carrousel.js
@@ -1,3 +1,21 @@
 (function(){
-    console.log("carrousel")
-})
+    console.log('carrousel');
+    let elBtn = document.querySelector(".bouton");
+    console.log(elBtn);
+    let elCarrousel = document.querySelector(".carrousel");
+    let elCarrousel__x = document.querySelector(".carrousel__x");
+    let elGalerie = document.querySelector(".galerie");
+    let elGalerieImgs = document.querySelectorAll(".galerie img")
+    elBtn.addEventListener('mousedown', function(){
+        console.log("evenement");
+        elCarrousel.classList.add("carrousel--ouvrir");
+        for (const elImg of elGalerieImgs){
+            console.log(elImg.getAttribute('src'));
+        }
+    })
+
+    elCarrousel__x.addEventListener('mousedown', function(){
+        elCarrousel.classList.remove("carrousel--ouvrir");
+
+    })
+})();
